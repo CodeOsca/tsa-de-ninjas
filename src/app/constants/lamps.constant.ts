3045c07791abf6a8d3489460fac906774286b94c
CodeOscar <oscarcolinafs@gmail.com>
2022-08-09 20:53:52 -0400

    link de afiliados cambiados en camisetas y lamparas

--- src/app/constants/lamps.constant.ts
+++ src/app/constants/lamps.constant.ts
@@ -2,101 +2,34 @@ import { Card } from '../pages/shared/interfaces/card';
 
 export const lamps2: Card[] =
     [
+
+
         {
-            title: 'JYDNBGLS 3D ilusión luz LED noche Gaara estatuilla japonesa anime',
-            subtitle: 'Naruto lámpara Otaku dormitorio decoración manga arte regalo hogar oficina lámpara decoración niños s cumpleaños',
-            link: 'https://www.amazon.es/JYDNBGLS-estatuilla-dormitorio-decoraci%C3%B3n-cumplea%C3%B1os/dp/B098LJDFDG/ref=sr_1_19?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358403&s=kitchen&sr=1-19',
-            imgUrl: 'https://m.media-amazon.com/images/I/61+YVNDJaES._AC_UL320_.jpg'
+            title: 'lámpara de Escritorio Presente de cumpleaños de los niños Juguetes Minato Naruto Luz De Humor',
+            subtitle: 'LIX-XYD Luz Ambiente, Noche llevó la lámpara lámpara del Humor del Cambio de 7 Colores Creativos',
+            link: 'https://amzn.to/3BSHa7s',
+            imgUrl: 'https://m.media-amazon.com/images/I/61hkcwDwGKL._AC_UL320_.jpg'
         },
 
         {
-            title: 'JYDNBGLS Lámpara 3D Anime Naruto Kakashi Hatake Figura japonesa',
-            subtitle: 'LED noche luz Icha Icha Paradaisu noche para niños dormitorio decoración escritorio 16 colores',
-            link: 'https://www.amazon.es/JYDNBGLS-Paradaisu-dormitorio-decoraci%C3%B3n-escritorio/dp/B098LKQSV8/ref=sr_1_20?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358403&s=kitchen&sr=1-20',
-            imgUrl: 'https://m.media-amazon.com/images/I/61RYJe0rc8S._AC_UL320_.jpg'
+            title: 'Juguete de luz de noche de anime 3D, 4 patrones, 16 cambios de color con control remoto y tacto',
+            subtitle: 'Regalos de cumpleaños para niños y niñas, adecuado para decoración del hogar',
+            link: 'https://amzn.to/3QAySWb',
+            imgUrl: 'https://m.media-amazon.com/images/I/71jAc2RZ6fL._AC_UL320_.jpg'
         },
 
         {
-            title: '3D Illusion Night Light Vortex Naruto Led Night Light 7 Cambio De Color Control Remoto',
-            subtitle: 'Lámpara De Escritorio Usb Hogar Dormitorio Lámpara De Noche Juguetes Para Niños Regalo De Cumpleaños De Navidad',
-            link: 'https://www.amazon.es/Illusion-Escritorio-Dormitorio-Juguetes-Cumplea%C3%B1os/dp/B084TYN1X4/ref=sr_1_29?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-29',
-            imgUrl: 'https://m.media-amazon.com/images/I/61DzBUGoXbL._AC_UL320_.jpg'
+            title: '7 Colores - Blast Jian Hao Yasuo，Luz De Noche 3D，Luz De Sueño Usb',
+            subtitle: 'Flash Led，Decoración De La Habitación De Los Niños，Regalo De Navidad De Halloween',
+            link: 'https://amzn.to/3zHjfW5',
+            imgUrl: 'https://m.media-amazon.com/images/I/61LzNJ28sBL._AC_UL320_.jpg'
         },
 
         {
-            title: 'Anime Naruto Akatsuki Logo',
-            subtitle: 'Lámpara 3D para habitación infantil (acrílico), diseño con logotipo de Naruto D',
-            link: 'https://www.amazon.es/Anime-Naruto-Akatsuki-Logo-habitaci%C3%B3n/dp/B088MBHNCN/ref=sr_1_33?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-33',
-            imgUrl: 'https://m.media-amazon.com/images/I/61pKxx9uzmL._AC_UL320_.jpg'
+            title: 'Lampara de batman',
+            subtitle: 'Paladone Lámpara Figura Batman, gris y negra (PP6376BM)',
+            link: 'https://amzn.to/3djxMQi',
+            imgUrl: 'https://m.media-amazon.com/images/I/61vtdyRQEUL._AC_UL320_.jpg'
         },
 
-        {
-            title: 'Haili Luces Decorativas/Noche llevó lámpara del Humor del Cambio de 7 Colores Creativo',
-            subtitle: 'Lámpara de Escritorio Presente de cumpleaños de los niños Juguetes Minato Naruto',
-            link: 'https://www.amazon.es/Decorativas-Creativo-Escritorio-Presente-cumplea%C3%B1os/dp/B08LHHMKF5/ref=sr_1_25?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-25',
-            imgUrl: 'https://m.media-amazon.com/images/I/61zt+7h-P7L._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Anime Naruto 3D lámpara Uchiha spot',
-            subtitle: 'LED noche luz dormitorio niños lámpara de mesa decoración regalo cumpleaños 16 colores',
-            link: 'https://www.amazon.es/l%C3%A1mpara-dormitorio-decoraci%C3%B3n-cumplea%C3%B1os-colores/dp/B09CLNJ716/ref=sr_1_28?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-28',
-            imgUrl: 'https://m.media-amazon.com/images/I/614blBb-9tL._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Lámpara De Ilusión Óptica 3D-Anime Naruto Carácter',
-            subtitle: 'Luz Led-Luz Nocturna Para Niños Decoración Del Hogar Luz Regalo-Control Remoto',
-            link: 'https://www.amazon.es/3D-Anime-Car%C3%A1cter-Nocturna-Decoraci%C3%B3n-Regalo-Control/dp/B08MZT4BTT/ref=sr_1_27?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-27',
-            imgUrl: 'https://m.media-amazon.com/images/I/61HWxLKEUBL._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Lámpara De Ilusión Óptica 3D-Dibujos Animados Anime Naruto Boy',
-            subtitle: 'Lámpara Led-Luz Nocturna Para Niños Lámpara De Decoración Del Hogar-El Regalo Más Genial-Control Remoto',
-            link: 'https://www.amazon.es/3D-Dibujos-Animados-Nocturna-Decoraci%C3%B3n-Genial-Control/dp/B08KYDL9CF/ref=sr_1_29?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-29',
-            imgUrl: 'https://m.media-amazon.com/images/I/61UDKG-HzOL._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Lámpara De Ilusión Óptica 3D-Dibujos Animados Anime Naruto Boy',
-            subtitle: 'Lámpara Led-Luz Nocturna Para Niños Lámpara De Decoración Del Hogar-El Regalo Más Genial-Control Remoto',
-            link: 'https://www.amazon.es/3D-Dibujos-Animados-Nocturna-Decoraci%C3%B3n-Genial-Control/dp/B08KYD3R1X/ref=sr_1_30?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-30',
-            imgUrl: 'https://m.media-amazon.com/images/I/616gFf2xs0L._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Luz nocturna LED de doble color, mitad cara, Obito Uchiha y Kakashi Hatake',
-            subtitle: 'Luz nocturna para niños, dormitorio, decoración, lámpara de escritorio de Naruto',
-            link: 'https://www.amazon.es/nocturna-cacashi-dormitorio-decoraci%C3%B3n-escritorio/dp/B088M9MPZS/ref=sr_1_34?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-34',
-            imgUrl: 'https://m.media-amazon.com/images/I/614kZ5B0irL._AC_UL320_.jpg'
-        },
-
-        {
-            title: '7 Colores Que Cambian 3D Naruto Modelado Visual',
-            subtitle: 'Led Anime Night Lights Kids Touch Button Lámpara Usb Lámpara De Mesa Decoración Para El Hogar Iluminación',
-            link: 'https://www.amazon.es/Colores-Cambian-Modelado-Decoraci%C3%B3n-Iluminaci%C3%B3n/dp/B084TYRQ4Q/ref=sr_1_36?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-36',
-            imgUrl: 'https://m.media-amazon.com/images/I/61wZaIsdnNL._AC_UL320_.jpg'
-        },
-
-        {
-            title: 'Luz De Noche Para Niños 3D Naruto Y I Love Luo Luz De Ilusión Led Luz De Humor 7 Colores Control Remoto Táctil',
-            subtitle: 'Lámpara De Mesa Usb Lámpara De Mesilla Para Niños Regalo De Cumpleaños De Navidad',
-            link: 'https://www.amazon.es/Ilusi%C3%B3n-Colores-Control-L%C3%A1mpara-Cumplea%C3%B1os/dp/B084TYNYM8/ref=sr_1_37?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644358987&s=kitchen&sr=1-37',
-            imgUrl: 'https://m.media-amazon.com/images/I/61tAt5QR1eL._AC_UL320_.jpg'
-        },
-
-        {
-            title: '3D LED Luz de la Noche Naruto Sasuke Itachi Figura de Acción',
-            subtitle: '16 Colores Touch Illusion Optical Table Lamp Home Decoration Model',
-            link: 'https://www.amazon.es/Naruto-Colores-Illusion-Optical-Decoration/dp/B0888XWB7V/ref=sr_1_54?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644359593&s=kitchen&sr=1-54',
-            imgUrl: 'https://m.media-amazon.com/images/I/51QojoYXTRL._AC_UL320_.jpg'
-        },
-
-        {
-            title: '3D Led Sasuke Naruto Accesorios De Iluminación De Luz Nocturna',
-            subtitle: '7 Colores Cambiantes Para Niños Mesita De Noche Visual Anime Usb Lámpara De Escritorio',
-            link: 'https://www.amazon.es/Accesorios-Iluminaci%C3%B3n-Nocturna-Cambiantes-Escritorio/dp/B0837JY8H5/ref=sr_1_77?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=lampara+naruto&qid=1644359674&s=kitchen&sr=1-77',
-            imgUrl: 'https://m.media-amazon.com/images/I/41xkwhknO5L._AC_UL320_.jpg'
-        }
     ]
