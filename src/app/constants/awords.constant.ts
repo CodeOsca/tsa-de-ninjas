d3d7219c75000040f80961ef79189ef8e4817f98
CodeOscar <oscarcolinafs@gmail.com>
2022-08-12 19:47:07 -0400

    contenido agregado a la pagina de camisetas y enlaces de afiliados agregados a espadas y sudaderas

--- src/app/constants/awords.constant.ts
+++ src/app/constants/awords.constant.ts
@@ -7,23 +7,15 @@ export const awords2: Card[] = [
         subtitle: 'Forma Parte de la línea de Disfraces para niños',
         imgUrl: 'https://m.media-amazon.com/images/I/81+eR2h-gqL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/Lion-4450000-Espada-Vikingo-Colour/dp/B010ZZ87ZY/ref=sr_1_11?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-11'
+        link: 'https://amzn.to/3BZNjPv'
     },
 
     {
-        title: "Rubie's - Espada de caballero o rey medieval",
-        subtitle: "Accesorio disfraz (Rubie's 376)",
-        imgUrl: 'https://m.media-amazon.com/images/I/51RDEgtDuXL._AC_UL320_.jpg',
-        isProduct: true,
-        link: 'https://www.amazon.es/Rubies-RUB376ACC-Espada-medieval-376/dp/B000H3UIEI/ref=sr_1_16?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-16'
-    },
-
-    {
-        title: 'Boland 00696 Piratas Espada',
+        title: 'WDK Partner A1303140 - Espada ninja luminosa',
         subtitle: 'Unisex de niños, 37 cm',
-        imgUrl: 'https://m.media-amazon.com/images/I/51wWnikgShL._AC_UL320_.jpg',
+        imgUrl: 'https://m.media-amazon.com/images/I/31+F0sZtQFL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/BOLAND-00696-Piratas-Espada-unisex-ni%C3%B1os/dp/B072F841BJ/ref=sr_1_17?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-17'
+        link: 'https://amzn.to/3JOGthH'
     },
 
     {
@@ -31,23 +23,23 @@ export const awords2: Card[] = [
         subtitle: 'color/modelo surtido',
         imgUrl: 'https://m.media-amazon.com/images/I/71diS2L+qjL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/Sancto-2728Q-Disfraz-para-adulto/dp/B008P947AM/ref=sr_1_18?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-18'
+        link: 'https://amzn.to/3bPc2Lx'
     },
 
     {
-        title: 'Viving Disfraces Viving 204819',
-        subtitle: 'Ninja Set (42 cm, Talla única)',
-        imgUrl: 'https://m.media-amazon.com/images/I/71QG+5UwOxL._AC_UL320_.jpg',
+        title: 'Amosfun Espada japonesa de plástico samurai juguete',
+        subtitle: 'Espada Halloween vestir accesorios de juguete para niños (tamaño pequeño)',
+        imgUrl: 'https://m.media-amazon.com/images/I/51cORtCWIYL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/Viving-Disfraces-204819-Ninja-Talla/dp/B071VK4T3Z/ref=sr_1_22?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-22'
+        link: 'https://amzn.to/3pgeY73'
     },
 
     {
-        title: 'Boland 00659',
-        subtitle: 'Espada Pirata con Vaina, Aprox. 52 cm',
-        imgUrl: 'https://m.media-amazon.com/images/I/51xBNSUnI5L._AC_UL320_.jpg',
+        title: 'ATOSA Espada plateada con agarre dorado y negro',
+        subtitle: 'Espada con Vaina, Aprox. 63,5 x 12 cm',
+        imgUrl: 'https://m.media-amazon.com/images/I/419RTKdCBRL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/Boland-00659-Pirata-Espada-aprox/dp/B010NRV6QQ/ref=sr_1_27?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=309TD01PI49YZ&keywords=espada+ninja&qid=1644622665&sprefix=espada+ninjas%2Caps%2C706&sr=8-27'
+        link: 'https://amzn.to/3QDOKXR'
     },
 
     {
@@ -55,15 +47,15 @@ export const awords2: Card[] = [
         subtitle: '(Madera de Roble japonés)',
         imgUrl: 'https://m.media-amazon.com/images/I/71h943RxFpL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/DEPICE-w-boj-Bokken-entrenamiento-japon%C3%A9s/dp/B00766X5E4/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1NUVV26LPCFF2&keywords=espada+de+bambu&qid=1644623356&sprefix=espadas+de+ambu%2Caps%2C845&sr=8-1'
+        link: 'https://amzn.to/3SPwRqM'
     },
 
     {
-        title: 'DEPICE W-BOPG Bokken',
-        subtitle: 'Unisex Adulto, Naturaleza, Uni',
-        imgUrl: 'https://m.media-amazon.com/images/I/618VZSItc-L._AC_UL320_.jpg',
+        title: 'GHCXY Arma de Espada de Madera Katanas Modelo de Arma de Espada Samurai Sword Anime Ninja Katana con Vaina',
+        subtitle: 'Juguete de Arma Decorativa de Cosplay, para Halloween Carnival/75Cm',
+        imgUrl: 'https://m.media-amazon.com/images/I/61pK6vcIJZL._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/DEPICE-W-BOPG-Bokken-Unisex-Naturaleza/dp/B08ZDNYYK2/ref=sr_1_27?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1NUVV26LPCFF2&keywords=espada+de+bambu&qid=1644623356&sprefix=espadas+de+ambu%2Caps%2C845&sr=8-27'
+        link: 'https://amzn.to/3QGjd7H'
     },
 
     {
@@ -71,6 +63,24 @@ export const awords2: Card[] = [
         subtitle: 'Unisex Adulto',
         imgUrl: 'https://m.media-amazon.com/images/I/618VZSItc-L._AC_UL320_.jpg',
         isProduct: true,
-        link: 'https://www.amazon.es/DEPICE-Waffe-Unisex-Adulto-Naturaleza/dp/B00WRBVWWK/ref=sr_1_28?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1NUVV26LPCFF2&keywords=espada+de+bambu&qid=1644623356&sprefix=espadas+de+ambu%2Caps%2C845&sr=8-28'
+        link: 'https://amzn.to/3AjuOEk'
+    },
+
+    {
+        title: 'GHCXY Espada de Arma de Espada de Woodennife Samurai Samurai',
+        subtitle: 'Espada Ninja Samurai Negra de Anime con Scabbard, Katana Sword Weapon Props Anime Ninja Sword Toy, Fanáticos de Anime Katanas Hechas a M',
+        imgUrl: 'https://m.media-amazon.com/images/I/61ucicLhfrL._AC_UL320_.jpg',
+        isProduct: true,
+        link: 'https://amzn.to/3pgfrpP'
+    },
+
+    {
+        title: 'GHCXY Espada de Madera',
+        subtitle: 'Arma de Arma de Arma Japonesa Katanas Pu Espuma Samurai Espada con Anime de Anime Ninja Espada Juguete para Ninja Soldado Guerrero/Hashibira Inosuke/100Cm',
+        imgUrl: 'https://m.media-amazon.com/images/I/41QD2hYNkbL._AC_UL320_.jpg',
+        isProduct: true,
+        link: 'https://amzn.to/3bPKtBZ'
     }
 ]
+
+
--- src/app/constants/sweeatshirts.constant.ts
+++ src/app/constants/sweeatshirts.constant.ts
@@ -5,14 +5,14 @@ export const sweeatsHirts2: Card[] =
         {
             title: 'Shirtzshop - Sudadera con Capucha',
             subtitle: 'Diseño con Texto en alemán Weil Multitasking Ninja',
-            link: 'https://www.amazon.es/Shirtzshop-Sudadera-capucha-dise%C3%B1o-Multitasking/dp/B07GDMFFSB/ref=sr_1_30?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=20GZ9JLLMJ1WY&keywords=sudaderas+ninja&qid=1644538129&sprefix=sudaderas+ninjas%2Caps%2C270&sr=8-30',
+            link: 'https://amzn.to/3ducDmr',
             imgUrl: 'https://m.media-amazon.com/images/I/61P-at4SnCL._AC_UL320_.jpg'
         },
 
         {
             title: 'Desconocido Sudadera con Capucha Unisex',
             subtitle: 'Ninja Dad, Padre, Padre, Padre, Padre, Abuelo, Luchador, Guerra, Soldado',
-            link: 'https://www.amazon.es/Sudadera-capucha-unisex-luchador-soldado/dp/B07KPL1P4Z/ref=sr_1_32?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=20GZ9JLLMJ1WY&keywords=sudaderas+ninja&qid=1644538129&sprefix=sudaderas+ninjas%2Caps%2C270&sr=8-32',
+            link: 'https://amzn.to/3C8hpQZ',
             imgUrl: 'https://m.media-amazon.com/images/I/51cP6RSNXkL._AC_UL320_.jpg'
         },
 
@@ -24,23 +24,30 @@ export const sweeatsHirts2: Card[] =
         },
 
         {
-            title: 'MELKEVDY Naruto Sudaderas con Capucha',
-            subtitle: 'Hermanos Uchiha Itachi Uchiha Sasuke Pullover Unisex Harajuku Chándal',
-            link: 'https://www.amazon.es/Sudaderas-Capucha-Hermanos-Pullover-Harajuku/dp/B09NY2W6FH/ref=sxin_14_slsr_d_i_fsnewarrivals_fa_0_B09NY2W6FH?cv_ct_cx=sudaderas+naruto&keywords=sudaderas+naruto&pd_rd_i=B09NY2W6FH&pd_rd_r=bedde0fe-6fdb-4a63-9e89-66a6aaa1b657&pd_rd_w=lR9bq&pd_rd_wg=fJA60&pf_rd_p=d2738a78-1ce2-4cfa-a149-885d95e8b159&pf_rd_r=HYA0STD30WC3JNBJRRW5&psc=1&qid=1644538560&sprefix=Sudaderas+narut%2Caps%2C1581&sr=1-1-5b2ee5ec-0735-4782-9f3a-9d21a55133d0',
-            imgUrl: 'https://m.media-amazon.com/images/I/61VQhA3Fs+L._AC_UL320_.jpg'
+            title: 'Spy Ninjas Merch Fashion',
+            subtitle: 'Sudaderas unisex para niños y niñas',
+            link: 'https://amzn.to/3JSkFBx',
+            imgUrl: 'https://m.media-amazon.com/images/I/518OihtzEVL._AC_UL320_.jpg'
         },
 
         {
-            title: 'aige Unisex Naruto 3D Print Pullover Hoodie Sudadera',
-            subtitle: 'Anime Naruto Cosplay Disfraz para Hombres Mujeres Niños y Niñas',
-            link: 'https://www.amazon.es/Baige-Pullover-Sudadera-Cosplay-Disfraz/dp/B07X7MY5S4/ref=sr_1_3?keywords=sudaderas%2Bnaruto&qid=1644538560&sprefix=Sudaderas%2Bnarut%2Caps%2C1581&sr=8-3&th=1&psc=1',
-            imgUrl: 'https://m.media-amazon.com/images/I/71KO953uPOL._AC_UL320_.jpg'
+            title: 'Ninja Kidz - Sudadera con capucha para niños y niñas',
+            subtitle: '',
+            link: 'https://amzn.to/3C8hMel',
+            imgUrl: 'https://m.media-amazon.com/images/I/514XG10753L._AC_UL320_.jpg'
         },
 
         {
             title: 'Desconocido Revenge Is Sweet Ninja',
             subtitle: 'Pixel Art Sudadera con Capucha Unisex',
-            link: 'https://www.amazon.es/Desconocido-Revenge-Sudadera-Capucha-Unisex/dp/B089MB8CQW/ref=sr_1_28?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=20GZ9JLLMJ1WY&keywords=sudaderas+ninja&qid=1644538129&sprefix=sudaderas+ninjas%2Caps%2C270&sr=8-28',
+            link: 'https://amzn.to/3QH4wBf',
             imgUrl: 'https://m.media-amazon.com/images/I/516EsM3TWWL._AC_UL320_.jpg'
+        },
+
+        {
+            title: 'Sudadera de estrella ninja',
+            subtitle: 'Urban Backwoods Ninja I Sudadera para Hombre Sweatshirt Pullover',
+            link: 'https://amzn.to/3JS8Lrw',
+            imgUrl: 'https://m.media-amazon.com/images/I/51E+cEDcziL._AC_UL320_.jpg'
         },
     ]
